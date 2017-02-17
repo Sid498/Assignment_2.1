5fa43b1bc75af3c9a122946df63fdf00d06a1844
Sid498 <sidpawar619@gmail.com>
2017-02-17 19:21:45 +0530

    Assignment_2.1

---
+++ Assignment_2.1/src/MyName.java
@@ -0,0 +1,17 @@
+
+public class MyName {
+
+	public static void main(String[] args) {
+		//This is the First way using For Loop
+		for (int i = 1; i<2;System.out.println("Siddhesh")) {
+			i++;
+		}
+		//This is the second way using if statement
+		if(System.out.printf("Siddhesh")==null)
+		{
+			
+		}
+
+	}
+
+}
